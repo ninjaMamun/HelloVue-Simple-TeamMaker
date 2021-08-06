7d008faec61ad781e8d8bb54e117de61bea3ed62
Shahriyar Mamun <mamun@Shahriyars-MacBook-Air.local>
2021-08-07 02:52:07 +0600

    simple team maker app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="d-flex justify-content-center ">
+    <div class="">
+      <h1 class="mt-5 text-info">Dream BD Cricket Team</h1>
+      <h4 class="text-center mt-4">Total Player: {{ playersCount }}</h4>
+      <div class="text-center m-4">
+        <span v-for="(hero, index) in bdTeam" v-bind:key="hero.name">
+          <div>{{ hero.name }} <button type="button" class="close" aria-label="Close" @click="remove(index)"><span aria-hidden="true">&times;</span></button></div>
+        </span>
+      </div>
+
+      <form class="text-center" @submit.prevent="addPlayer">
+        <input class="form-control mb-4" v-model="newHero" placeholder="Player Name Here" />
+        <button type="submit"  class="btn btn-outline-success">Add Player</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    playersCount() {
+      return this.bdTeam.length;
+    },
+  },
+
+  methods: {
+    addPlayer() {
+      if (this.newHero !== "") {
+        this.bdTeam.push({ name: this.newHero });
+        this.newHero = "";
+      } else {
+        alert("Enter Player Name");
+      }
+    },
+    remove(index) {
+      this.bdTeam = this.bdTeam.filter((hero, i) => i != index);
+    },
+  },
+
+  data() {
+    return {
+      newHero: "Mamun",
+
+      bdTeam: [
+        { name: "Sakib Al Hasan" },
+        { name: "Mustafizur Rahman" },
+        { name: "Mahmudullah" },
+        { name: "Tamim Iqbal" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
